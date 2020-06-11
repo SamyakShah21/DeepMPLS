b3f576ebf306f7bd3da799745a390e69fec8b49c
SamyakShah21 <53524752+SamyakShah21@users.noreply.github.com>
2020-06-11 16:31:13 +0530

    Create coordinate.cpp

---
+++ utils/coordinate.cpp
@@ -0,0 +1,20 @@
+#include "coordinate.h"
+#include <cmath>
+
+namespace aalwines {
+
+    \\Using the 'Coordinate' class from "coordinate.h"	
+    Coordinate::Coordinate(double latitude, double longitude) : _latitude(latitude), _longitude(longitude),
+        _rad_lat(M_PI / 180 * latitude), _rad_long(M_PI / 180 * longitude) {}
+
+    \\adding to the distane_to function defined in the Coordinate class in "coordinate.h"	
+    double Coordinate::distance_to(const Coordinate& other) const {
+        // Using the Haversine's distance formula.
+        return 2* _R_km  * std::asin(std::sqrt(std::pow((_rad_lat - other._rad_lat)/2, 2) +
+        std::cos(_rad_lat) * std::cos(other._rad_lat) * std::pow(std::sin((_rad_long - other._rad_long) / 2), 2)));
+    }
+
+    void Coordinate::write_xml_attributes(std::ostream& s) const {
+        s << " latitude=\"" << _latitude << "\" longitude=\"" << _longitude << "\" ";
+    }
+}
