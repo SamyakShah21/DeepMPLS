0e5193cfdee8ca67b30698b19b4f8f9ad93ffe4c
SamyakShah21 <53524752+SamyakShah21@users.noreply.github.com>
2020-06-11 17:02:33 +0530

    Create outcome.h

---
+++ utils/outcome.h
@@ -0,0 +1,9 @@
+#ifndef OUTCOME_H
+#define OUTCOME_H
+
+namespace utils {
+    //defining an enum (enumerate) object to print the output in the final file.
+    enum outcome_t { YES, NO, MAYBE };    
+}
+
+#endif //OUTCOME_H 
